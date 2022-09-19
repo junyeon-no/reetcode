4a72daad89d66feb2947499fd9c08a321c61cfc5
노준현 <nojunhyeon@nojunhyeon-ui-MacBookAir.local>
2022-09-19 14:57:53 +0900

    11번 -> reetcode 238 추가

---
+++ 11_238_Product_of_Array_Except_Self.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        rst = []
+        for i in range(len(nums)):
+            gob = 1
+            for j,n in enumerate(nums):
+                if j == i : continue
+                gob*=n
+            rst.append(gob)
+        return rst
+
+class Solution2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        rst = []
+        p = 1
+        #왼쪽 곱셈
+        for i in range(0, len(nums)):
+            rst.append(p)
+            p = p*nums[i]
+        p=1
+        #왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+        for i in range(len(nums) -1, 0-1, -1):
+            rst[i] = rst[i] * p
+            p = p * nums[i]
+        return rst
+
+nums =  [1,2,3,4]
+s = Solution2()
+print(s.productExceptSelf(nums))
